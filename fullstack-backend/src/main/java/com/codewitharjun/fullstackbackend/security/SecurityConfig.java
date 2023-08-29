367faa857b87c977b33c3c08091e1ba62306fb09
moncef <72748046+somoncef@users.noreply.github.com>
2023-08-29 15:19:43 +0000

    29 3:19 pm

--- fullstack-backend/src/main/java/com/codewitharjun/fullstackbackend/security/SecurityConfig.java
+++ fullstack-backend/src/main/java/com/codewitharjun/fullstackbackend/security/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
                 .antMatchers("/auth/login").permitAll()
+                .antMatchers("/user").permitAll()
                 .anyRequest().authenticated();
     }
 
