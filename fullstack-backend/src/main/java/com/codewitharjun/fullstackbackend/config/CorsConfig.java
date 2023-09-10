82dd75ff3505aa5ecabcef4fe7f5a59306b5f444
moncef <72748046+somoncef@users.noreply.github.com>
2023-09-10 18:09:29 +0000

    g

--- fullstack-backend/src/main/java/com/codewitharjun/fullstackbackend/config/CorsConfig.java
+++ fullstack-backend/src/main/java/com/codewitharjun/fullstackbackend/config/CorsConfig.java
@@ -2,6 +2,7 @@ package com.codewitharjun.fullstackbackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -11,24 +12,30 @@ public class CorsConfig {
 
     @Bean
     public CorsFilter corsFilter() {
+        System.out.println("Setting up CORS filter");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
 
         config.addAllowedOrigin("http://localhost:3000");
 
+        // Allow various HTTP methods
         config.addAllowedMethod("GET");
         config.addAllowedMethod("POST");
         config.addAllowedMethod("PUT");
         config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod(HttpMethod.OPTIONS);
 
+        config.addAllowedHeader("Origin");
         config.addAllowedHeader("Content-Type");
         config.addAllowedHeader("Authorization");
+        config.addAllowedHeader("Accept");
+        config.addAllowedHeader("Access-Control-Request-Method");
+        config.addAllowedHeader("Access-Control-Request-Headers");
 
         config.setAllowCredentials(true);
 
         source.registerCorsConfiguration("/**", config);
-
-        System.out.println("CorsFilter Applied");
 
         return new CorsFilter(source);
     }
